96db7df3ff778a4f30f0c66f21e375b1686431c5
opportify-bb <191276444+opportify-bb@users.noreply.github.com>
2025-11-11 07:10:30 +0000

    chore: update version to 0.3.1 in pyproject.toml and setup.py (#10)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md') as readme_file:
     readme = readme_file.read()
 
 NAME = "opportify_sdk"
-VERSION = "0.2.0"
+VERSION = "0.3.1"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
